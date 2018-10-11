74bda894c22f480aad96b44ff316f5621ab4e9a7
Jessica Chao <jchao@brg.com>
2018-10-10 18:21:36 -0700

    4-user-sign-in-sign-out WIP

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,8 +1,8 @@
 module.exports = {
   validateUsers(req, res, next) {
     if(req.method === "POST") {
+      req.checkBody("name", "must be valid").isLength({min: 6});
       req.checkBody("email", "must be valid").isEmail();
-      req.checkBody("name", "must be at least 6 characters in length").isLength({min: 6});
       req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6});
       req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
     }
